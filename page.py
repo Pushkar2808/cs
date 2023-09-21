10bd45563d667ccdbd59a65133b0465940489335
ujjwalp360 <145079274+ujjwalp360@users.noreply.github.com>
2023-09-21 23:18:04 +0530

    Update page.py

--- page.py
+++ page.py
@@ -104,9 +104,10 @@ def showstock():
         r=y2-y1,m2-m1
         b=w-(r[0]*12+r[1])
         S=d[0][6]
+        C=d[0][5]
         st.write(f"warranty period:{w} months({b} months remaining)")
         st.write(f"specification:{S}")
-        st.write(f"condition:{d[0][5]}")
+        st.write(f"condition:{C}")
         if st.button("back"):
                 st.session_state.page = "op"
     elif st.button("back"):
