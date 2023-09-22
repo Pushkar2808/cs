1d44522864837a41645ca1bbff487717b45bce2b
ujjwalp360 <145079274+ujjwalp360@users.noreply.github.com>
2023-09-22 11:51:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import mysql.connector
 from page import *
-
+db=mysql.connector.connect(host="bakgs4ewc5wjhjigbt9w-mysql.services.clever-cloud.com",user="u6yovg764kskss2z",password="iWSZjK9HdgGfMmdZEUJp",database="bakgs4ewc5wjhjigbt9w")
+c=db.cursor()
 page=st.session_state.get("page","login")
 if page == "login":
             login()
